5f7cefd7bad522ad09e6cf604af1fe28b363decf
Will Tuttle <willtuttle24@gmail.com>
2023-04-02 14:02:56 -0500

    Update BinarySearchTree.js

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,8 @@
 class BinarySearchTree {
-  // your code here
-}
+    constructor(key = null, value = null, parent = null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
